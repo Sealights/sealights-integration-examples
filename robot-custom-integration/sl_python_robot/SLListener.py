2863b1a7ba5698e6a792dcf50f9c11d4e8cd8deb
marcsealights <46889369+marcsealights@users.noreply.github.com>
2021-12-10 01:36:50 +0200

    Update SLListener.py

--- robot-custom-integration/sl_python_robot/SLListener.py
+++ robot-custom-integration/sl_python_robot/SLListener.py
@@ -28,11 +28,14 @@ class SLListener:
             self.labid = bsid
 
     def start_suite(self, suite, result):
+        if not suite.tests:
+            return
+        print(f'{SEALIGHTS_LOG_TAG} {len(suite.tests)} tests in suite {suite.longname}')
         """initialize the test session so that all the tests can be identified by SeaLights as being part of the same session"""
         self.create_test_session()
         """request the list of tests to be executed from SeaLights"""
         self.excluded_tests = set(self.get_excluded_tests())
-        print(f'>>>> Skipped tests: {list(self.excluded_tests)}')
+        print(f'{SEALIGHTS_LOG_TAG} {len(self.excluded_tests)} Skipped tests: {list(self.excluded_tests)}')
 
         """Narrow the test suite to only the recommended tests by Sealights"""
         all_tests = set()
@@ -42,7 +45,7 @@ class SLListener:
                 test.body.create_keyword(name="SKIP")
 
         tests_for_execution = list(all_tests - self.excluded_tests)
-        print(f'>>>> Tests for execution: {tests_for_execution}')
+        print(f'{SEALIGHTS_LOG_TAG} {len(tests_for_execution)} Tests for execution: {tests_for_execution}')
 
     def end_suite(self, date, result):
         if not self.test_session_id:
@@ -50,11 +53,14 @@ class SLListener:
         """Collect and report test results to SeaLights including start and end time"""
         test_results = self.build_test_results(result)
         if test_results:
-            requests.post(self.get_session_url(), json=test_results, headers=self.get_header())
-            print(f'>>>> {SEALIGHTS_LOG_TAG} Results: {test_results}')
+            print(f'{SEALIGHTS_LOG_TAG} {len(test_results)} Results to send: {test_results}')
+            response = requests.post(self.get_session_url(), json=test_results, headers=self.get_header())
+            if not response.ok:
+                print(f'{SEALIGHTS_LOG_TAG} Failed to upload results (Error {response.status_code})')
         """Close the test session"""
-        print(f'>>>> {SEALIGHTS_LOG_TAG} Deleting test session {self.test_session_id}')
+        print(f'{SEALIGHTS_LOG_TAG} Deleting test session {self.test_session_id}')
         requests.delete(self.get_session_url(), headers=self.get_header())
+        self.test_session_id = ''
 
     #--- Sealights API helpers ---
     
@@ -64,16 +70,16 @@ class SLListener:
 
         if not response.ok:
             print(
-                f'>>>> {SEALIGHTS_LOG_TAG} Failed to open Test Session (Error {response.status_code}),'
+                f'{SEALIGHTS_LOG_TAG} Failed to open Test Session (Error {response.status_code}),'
                 f' disabling Sealights Listener')
         else:
             res = response.json()
             self.test_session_id = res["data"]["testSessionId"]
-            print(f'>>>> {SEALIGHTS_LOG_TAG} Test session opened, testSessionId: {self.test_session_id}')
+            print(f'{SEALIGHTS_LOG_TAG} Test session opened, testSessionId: {self.test_session_id}')
 
     def get_excluded_tests(self):
         recommendations = requests.get(f'{self.get_session_url()}/exclude-tests', headers=self.get_header())
-        print(f'Recommendations: {recommendations.status_code}')
+        print(f'{SEALIGHTS_LOG_TAG} Retrieving Recommendations: {"OK" if recommendations.ok else "Error {recommendations.status_code}"}')
         if recommendations.status_code == 200:
             return recommendations.json()["data"]
         return []
